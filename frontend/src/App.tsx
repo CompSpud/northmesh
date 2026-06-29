8778b1216913d70026b4f901ade8a5c5f339e850
CompSpud <67025992+CompSpud@users.noreply.github.com>
2026-06-29 22:00:28 +0100

    Add initial repeater portal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import MapPage from './pages/MapPage'
 import NetworkPage from './pages/NetworkPage'
 import FlasherPage from './pages/FlasherPage'
+import PortalPage from './pages/PortalPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="map" element={<MapPage />} />
         <Route path="network" element={<NetworkPage />} />
         <Route path="flasher" element={<FlasherPage />} />
+        <Route path="portal" element={<PortalPage />} />
       </Route>
     </Routes>
   )
